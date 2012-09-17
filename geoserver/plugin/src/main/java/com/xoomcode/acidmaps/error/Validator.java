760bdb728d612a8a7ff843eff4d166e2d1d989e6
Stephan Plepelits <skunk@xover.mud.at>
2012-09-17 21:43:54 +0200

    Empty dataset is not an error

--- geoserver/plugin/src/main/java/com/xoomcode/acidmaps/error/Validator.java
+++ geoserver/plugin/src/main/java/com/xoomcode/acidmaps/error/Validator.java
@@ -5,7 +5,7 @@ import com.xoomcode.acidmaps.core.Configuration;
 public class Validator {
 
 	public static int validate(Configuration cfg) {
-		if(cfg.dataset == null || cfg.dataset.length == 0) return 1;
+		if(cfg.dataset == null) return 1;
 		if(cfg.datasetSize < 0) return 2;
 		if(cfg.simplifyMethod < 0 || cfg.simplifyMethod > 3) return 3;	
 		if(cfg.simplifySize < 0) return 4;
